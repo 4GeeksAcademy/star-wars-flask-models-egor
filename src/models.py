efc254d8b5fc4b5f9322a41b132002936286a062
Egor Ulybin <egorulybin@gmail.com>
2025-09-19 19:02:48 +0000

    proj submission 1

--- src/models.py
+++ src/models.py
@@ -1,15 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(20), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def serialize(self):
         return {
@@ -17,3 +20,44 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Character(db.Model):
+    __tablename__ = "characters"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    gender: Mapped[str] = mapped_column(String(10), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(10), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(10), nullable=False)
+
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    population: Mapped[int] = mapped_column(Integer, nullable=False)
+    diameter: Mapped[int] = mapped_column(Integer, nullable=False)
+    terrain: Mapped[str] = mapped_column(String(20), nullable=False)
+
+
+class Vehicle(db.Model):
+    __tablename__ = "vehicles"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    crew: Mapped[int] = mapped_column(Integer, nullable=False)
+    model: Mapped[int] = mapped_column(String(20), nullable=False)
+    manufacturer: Mapped[str] = mapped_column(String(40), nullable=False)
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
